2ee6b9c8cb36dcc7de3414b10f057cf37e771a7d
RameenMobin <88801689+RameenMobin@users.noreply.github.com>
2021-08-12 02:15:59 +0500

    Delete color_detector2.m

--- color_detector2.m
+++
@@ -1,8 +0,0 @@
-%choose any color from red, green, light blue, dark blue, yellow, orange,
-%purple
-color = 'red';  
-% type image name below
-image_name = 'image.png';
-out = color_detector(color, image_name);
-figure
-imshow(out)
